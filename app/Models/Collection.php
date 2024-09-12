923fd37f5b7cbafa61caead0602c4e3420f9357e
Anar Bayramov <anar.bayram@yahoo.com>
2024-09-12 14:52:45 +0400

    feat: Added models for project

---
+++ app/Models/Collection.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Collection extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+    ];
+
+    protected function images(): HasMany
+    {
+        return $this->hasMany(Image::class);
+    }
+}
---
+++ app/Models/Image.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Image extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'url'
+    ];
+
+    protected function collection(): BelongsTo
+    {
+        return $this->belongsTo(Collection::class);
+    }
+}
